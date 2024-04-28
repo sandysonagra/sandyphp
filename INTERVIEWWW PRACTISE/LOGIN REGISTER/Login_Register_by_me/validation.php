2d0f97d112b69ecd746fdb530721d9a233a6d6e5
sandip sonagra <sonagrasandip0963@gmail.com>
2024-04-28 21:55:32 +0530

    2155

--- INTERVIEWWW PRACTISE/LOGIN REGISTER/Login_Register_by_me/validation.php
+++ INTERVIEWWW PRACTISE/LOGIN REGISTER/Login_Register_by_me/validation.php
@@ -1,19 +1,32 @@
 <?php
 
-include('connection.php');
+include ('connection.php');
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$password = $_POST['password'];
+if (isset($_POST['submit'])) {
 
-if(isset($_POST['submit']))
-{
-    $sql = "INSERT INTO `login_register_by_me`(`name`, `email`, `password`) VALUES ('$name','$email','$password')";
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM `login_register_by_me` WHERE `email`='$email' ";
     $result = mysqli_query($conn, $sql);
+    $num_rows = mysqli_fetch_assoc($result);
 
-    if($result){
-        header('location:login.php');
+    if ($num_rows > 0) {
+        header('location:register.php');
+    } else {
+        $sql = "INSERT INTO `login_register_by_me`(`name`, `email`, `password`) VALUES ('$name','$email','$password')";
+        $result = mysqli_query($conn, $sql);
+
+        if ($result) {
+            header('location:login.php');
+        }
     }
+
 }
+// if(isset($_POST['submit']))
+// {
+//    
+// }
 
 ?>
